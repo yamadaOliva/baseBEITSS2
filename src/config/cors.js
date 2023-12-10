44aba718d6f324cf51eb661464556638feacc8a5
duyen.nguyen <duyen.nguyen@dssolution.jp>
2023-12-10 15:25:00 +0700

    feat: add cors ip deploy

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,7 +1,21 @@
 const configCors = (app) => {
     app.use(function (req, res, next) {
         // Website you wish to allow to connect
-        res.setHeader('Access-Control-Allow-Origin', process.env.PORT_REACT);
+        // res.setHeader('Access-Control-Allow-Origin', process.env.PORT_REACT);
+        const allowedOrigins = [
+            process.env.PORT_REACT,
+            'https://ii-shop-itss.vercel.app',
+            // Add more allowed origins as needed
+        ];
+
+        const origin = req.headers.origin;
+
+        // Check if the request origin is in the list of allowed origins
+        if (allowedOrigins.includes(origin)) {
+            // Allow the specific origin
+            res.setHeader('Access-Control-Allow-Origin', origin);
+        }
+
         // Request methods you wish to allow
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
         // Request headers you wish to allow
@@ -10,7 +24,7 @@ const configCors = (app) => {
         // to the API (e.g. in case you use sessions)
         res.setHeader('Access-Control-Allow-Credentials', true);
         // Pass to next layer of middleware
-        if('OPTIONS' === req.method){
+        if ('OPTIONS' === req.method) {
             res.sendStatus(200);
         }
         next();
