c4db424172ff9ef204d2678dd4d01c1cd9e80a1a
yamadaOliva <vietanh87.forwork@gmail.com>
2023-12-15 18:15:50 +0700

    test

--- src/seeders/fakerData.js
+++ src/seeders/fakerData.js
@@ -39,6 +39,31 @@ const getNRandom = (arr, n) => {
 
 const seedStores = async () => {
   // Delete all documents from Moduless collection
+  let content = ["hay", "tuyet", "tot", "xau", "binh thuong"];
+  let type = ["LIKE", "DISLIKE"];
+  let reactionData = [];
+  for (let i = 0; i <8; i++) {
+    reactionData.push({
+      id: i,
+      user_id: i+2,
+      type: type[Math.floor(Math.random() * 2)],
+      content: content[Math.floor(Math.random() * 5)],
+    });
+  }
+  let reviewData = []; 
+  for (let i = 0; i < 10; i++) {
+    let reaction = getNRandom(reactionData, 5);
+    reviewData.push({
+      id: i,
+      user_id: i,
+      content: faker.lorem.paragraph(),
+      rating: Math.floor(Math.random() * 5),
+      reactions: reaction,
+      likes: reaction.filter((item) => item.type === "LIKE").length,
+      dislikes: reaction.filter((item) => item.type === "DISLIKE").length,
+      images: [],
+    });
+  }
   const fs = require("fs");
   const data = fs.readFileSync("./src/seeders/store.json", "utf8");
   const dataTest = JSON.parse(data);
@@ -54,6 +79,7 @@ const seedStores = async () => {
   storeData = storeData.map((store) => {
     store.images = getNRandom(storeImage, 5+Math.floor(Math.random() * 5));
     store.products = getNRandom(productData, 10);
+    store.reviews = getNRandom(reviewData, 5);
     store.rating = 0;
     return store;
   });
